8de24efc4414564c8988051c0141b1c710f62b6f
Văn Đại <minichu1103@gmail.com>
2021-06-23 17:14:03 +0700

    new

---
+++ DoAn3/src/app/common/calendar-solar.spec.ts
@@ -0,0 +1,7 @@
+import { CalendarSolar } from './calendar-solar';
+
+describe('CalendarSolar', () => {
+  it('should create an instance', () => {
+    expect(new CalendarSolar()).toBeTruthy();
+  });
+});
--- DoAn3/src/app/components/calendar/calendar.component.ts
+++ DoAn3/src/app/components/calendar/calendar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CalendarLunar } from 'src/app/common/calendar-lunar';
+import { CalendarSolar } from 'src/app/common/calendar-solar';
 import { CalendarService } from 'src/app/services/calendar.service';
 
 @Component({
@@ -11,6 +12,7 @@ import { CalendarService } from 'src/app/services/calendar.service';
 })
 export class CalendarComponent implements OnInit {
   calendarLunar: CalendarLunar = new CalendarLunar;
+  calendarSolar: CalendarSolar = new CalendarSolar;
   constructor(private calendarService: CalendarService,
               private formBuilder: FormBuilder,
               private router: Router,
@@ -22,13 +24,9 @@ export class CalendarComponent implements OnInit {
       }),
     });
   ngOnInit(): void {
-    this.showCalendarLunar();
-      // this.checkoutFormGroup = this.formBuilder.group({
-      //   calendarSolar: this.formBuilder.group({
-      //     dateSolar: new FormControl('', [Validators.required,
-      //                                     Validators.minLength(8),]),
-      //   }),
-      // });
+      this.showCalendarLunar();
+      this.showCalendarSolar();
+
   }
   get dateSolar() {return this.checkoutFormGroup.get('calendarSolar.dateSolar');}
   showCalendarLunar() {
@@ -39,8 +37,14 @@ export class CalendarComponent implements OnInit {
     this.calendarLunar.day = tempCalendarLunar[0];
     this.calendarLunar.month = tempCalendarLunar[1];
     this.calendarLunar.year = tempCalendarLunar[2];
-    this.calendarLunar.id = tempCalendarLunar[3];
     console.log(this.calendarLunar.day);
+  }
+  showCalendarSolar() {
+    let handleDate: string[] = this.handleDate();
+    this.calendarSolar.day = parseInt(handleDate[1]);
+    this.calendarSolar.month = parseInt(handleDate[0]);
+    this.calendarSolar.year = parseInt(handleDate[2]);
+    console.log(this.calendarSolar.day);
   }
   handleDate(): string[] {
     let date = this.checkoutFormGroup.get('calendarSolar.dateSolar')?.value;
@@ -48,8 +52,11 @@ export class CalendarComponent implements OnInit {
     let handleDate2: string[] = handleDate.split("/");
     return handleDate2;
   }
+  isNumber(value: unknown) {
+    return Number.isNaN(value);
+  }
   onSubmit() {
    console.log(this.handleDate());
-   this.showCalendarLunar();
+   this.ngOnInit();
   }
 }
